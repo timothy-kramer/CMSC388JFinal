c908fff085ffad8d3e2aa0694793fcbac1368f87
Timothy Kramer <tkramer2@terpmail.umd.edu>
2025-05-09 21:27:56 -0400

    help plz

--- Final/flask_app/config.py
+++ Final/flask_app/config.py
@@ -1,4 +1,7 @@
 # Stores all configuration values
 import os
-SECRET_KEY = os.environ.get('SECRET_KEY')
-MONGODB_HOST = os.environ.get('MONGODB_HOST')
+# SECRET_KEY = os.environ.get('SECRET_KEY')
+# MONGODB_HOST = os.environ.get('MONGODB_HOST')
+
+SECRET_KEY = os.urandom(16)
+MONGODB_HOST = 'mongodb+srv://tkramer2:Halo@cluster0.pwvtz.mongodb.net/CMSc388J?retryWrites=true&w=majority&appName=Cluster0'
