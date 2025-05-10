0b0f0987035064ba16d47e2af9e3e09138fd47e5
Timothy Kramer <tkramer2@terpmail.umd.edu>
2025-05-09 20:59:21 -0400

    fix

--- Final/flask_app/config.py
+++ Final/flask_app/config.py
@@ -2,8 +2,3 @@
 import os
 SECRET_KEY = os.environ.get('SECRET_KEY')
 MONGODB_HOST = os.environ.get('MONGODB_HOST')
-# MONGODB_SETTINGS = {
-#     'db': 'CMSc388J',
-#     'host': 'mongodb+srv://tkramer2:Halo@cluster0.pwvtz.mongodb.net/CMSc388J?retryWrites=true&w=majority&appName=Cluster0'
-# }
-#MONGODB_HOST = 'mongodb+srv://tkramer2:Halo@cluster0.pwvtz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
