5a7dafd2b5f8b6b1da8030aef7ea9ea6dc5b7f10
Timothy Kramer <tkramer2@terpmail.umd.edu>
2025-05-09 21:31:07 -0400

    vercel plese cmon

--- Final/flask_app/config.py
+++ Final/flask_app/config.py
@@ -1,7 +1,8 @@
 # Stores all configuration values
 import os
-# SECRET_KEY = os.environ.get('SECRET_KEY')
-# MONGODB_HOST = os.environ.get('MONGODB_HOST')
+SECRET_KEY = os.environ.get('SECRET_KEY')
+MONGODB_HOST = os.environ.get('MONGODB_HOST')
+#print("code\n",os.urandom(16),"\n")
 
-SECRET_KEY = os.urandom(16)
-MONGODB_HOST = 'mongodb+srv://tkramer2:Halo@cluster0.pwvtz.mongodb.net/CMSc388J?retryWrites=true&w=majority&appName=Cluster0'
+# SECRET_KEY = os.urandom(16)
+# MONGODB_HOST = 'mongodb+srv://tkramer2:Halo@cluster0.pwvtz.mongodb.net/CMSc388J?retryWrites=true&w=majority&appName=Cluster0'
